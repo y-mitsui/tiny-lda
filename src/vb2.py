a77717a1735e9f6b958b5572170b5cfb7cdec465
Y-Mitsui <dyfomg@gmail.com>
2018-03-12 02:03:44 +0900

    parallel vb

--- src/vb2.py
+++ src/vb2.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import numpy as np
 from scipy.special import digamma
+import sys
 
 def normalize(ndarray, axis):
     return ndarray / ndarray.sum(axis = axis, keepdims = True)
@@ -24,9 +26,73 @@ def lhood(theta, phi, W, n_documents, n_topic):
             prob = np.dot(theta_hat[d], phi_hat[:, word_no])
             ret += np.log(prob) * c
     return ret
-                
-if __name__ == "__main__":
 
+class LDA:
+    def __init__(self, n_topic, n_iter, alpha=0.2, beta=0.1):
+        self.n_topic = n_topic
+        self.n_iter = n_iter
+        self.alpha0 = alpha
+        self.beta0 = beta
+    
+    def lhood(self, theta, phi, curpus):
+        phi_hat = np.zeros(phi.shape)
+        theta_hat = np.zeros(theta.shape)
+        n_documents = len(curpus)
+        for k in range(self.n_topic):
+            phi_hat[k] = phi[k] / np.sum(phi[k])
+        for d in range(n_documents):
+            theta_hat[d] = theta[d] / np.sum(theta[d])
+            
+        ret = 0.
+        for d, row_corpus in enumerate(curpus):
+            for w_i, w_c in row_corpus:
+                prob = np.dot(theta_hat[d], phi_hat[:, w_i])
+                ret += np.log(prob) * w_c
+        return ret
+        
+    def fit_transform(self, curpus):
+        D = len(curpus)
+        K = self.n_topic
+        
+        max_index = 0
+        for row_corpus in curpus:
+            document_max = 0
+            for w_i, w_c in row_corpus:
+                if document_max < w_i:
+                    document_max = w_i
+                    
+            if max_index < document_max:
+                max_index = document_max
+        V = max_index + 1
+        print(D, K, V)
+        
+        alpha = self.alpha0 + np.random.rand(D, K)
+        beta = self.beta0 + np.random.rand(K, V)
+        theta = normalized_random_array(D, K)
+        phi = normalized_random_array(K, V)
+        # estimate parameters
+        for t in range(self.n_iter):
+            dig_alpha = digamma(alpha) - digamma(alpha.sum(axis = 1, keepdims = True))
+            dig_beta = digamma(beta) - digamma(beta.sum(axis = 1, keepdims = True))
+
+            alpha_new = np.ones((D, K)) * self.alpha0
+            beta_new = np.ones((K, V)) * self.beta0
+            for (d, row_corpus) in enumerate(curpus):
+                q = np.zeros((V, K))
+                v = np.array(map(lambda x: x[0], row_corpus))
+                count = np.array(map(lambda x: x[1], row_corpus))
+                q[v, :] = (np.exp(dig_alpha[d, :].reshape(-1, 1) + dig_beta[:, v]) * count).T
+                q[v, :] /= q[v, :].sum(axis = 1, keepdims = True)
+
+                # alpha, beta
+                alpha_new[d, :] += count.dot(q[v])
+                beta_new[:, v] += count * q[v].T
+            alpha = alpha_new.copy()
+            beta = beta_new.copy()
+            print(t, self.lhood(alpha, beta, curpus))
+            
+if __name__ == "__main__":
+    np.random.seed(123456)
     # initialize parameters
     D, K, V = 1000, 2, 6
     alpha0, beta0 = 1.0, 1.0
@@ -45,7 +111,15 @@ if __name__ == "__main__":
     for (d, N_d) in enumerate(N):
         Z.append((np.random.rand(N_d, 1) < _theta[d, :]).argmax(axis = 1))
         W.append((np.random.rand(N_d, 1) < _phi[Z[-1], :]).argmax(axis = 1))
-
+ 
+    corpus = []
+    for (d, N_d) in enumerate(N):
+        v, count = np.unique(W[d], return_counts = True)
+        corpus.append(zip(v, count))
+    print(D, K, V)
+    lda = LDA(2, 100)
+    lda.fit_transform(corpus)
+    sys.exit(0)
     # estimate parameters
     T = 300
     for t in range(T):
@@ -54,10 +128,14 @@ if __name__ == "__main__":
 
         alpha_new = np.ones((D, K)) * alpha0
         beta_new = np.ones((K, V)) * beta0
-        for (d, N_d) in enumerate(N):
+        for (d, _) in enumerate(N):
+            row_corpus = corpus[d]
             # q
             q = np.zeros((V, K))
-            v, count = np.unique(W[d], return_counts = True)
+            v = np.array(map(lambda x: x[0], row_corpus))
+            count = np.array(map(lambda x: x[1], row_corpus))
+            
+            #v, count = np.unique(W[d], return_counts = True)
             q[v, :] = (np.exp(dig_alpha[d, :].reshape(-1, 1) + dig_beta[:, v]) * count).T
             q[v, :] /= q[v, :].sum(axis = 1, keepdims = True)
 
--- src/wiki.py
+++ src/wiki.py
@@ -1,69 +1,79 @@
 import MySQLdb
-from vb import LDA
+from parallel_vb import LDA
+#from vb import LDA
 import numpy as np
 from gensim import corpora, models, similarities
 import re
 import nltk
-from lda_wrap import LDAWrap
+import time
+import pickle
+#from lda_wrap import LDAWrap
 
 np.random.seed(12345)
-connection = MySQLdb.connect(db="nlp_dataset",user="root",passwd="password")
-connection.set_character_set('utf8')
-cursor = connection.cursor()
-cursor.execute("SELECT content FROM wikipedia_en ORDER BY id LIMIT 100")
-documents = []
-print "reading"
-for search_content, in cursor.fetchall():
-    documents.append(search_content)
 
-symbols = "<>^[]!-/:@-`{-~,;*+|=.#$%&?\\"
-numeric = re.compile("^[0-9\.%:,]+$")
-numeric2 = re.compile("[0-9]")
-double_quote = re.compile("\"([A-Za-z0-9]+)\"")
-single_quote = re.compile("'([A-Za-z0-9]+)'")
-kakko_quote = re.compile("\(([A-Za-z0-9]+)\)")
+if False:
+    connection = MySQLdb.connect(db="nlp_dataset",user="root",passwd="password")
+    connection.set_character_set('utf8')
+    cursor = connection.cursor()
+    cursor.execute("SELECT content FROM wikipedia_en ORDER BY id LIMIT 40000")
+    documents = []
+    print "reading"
+    for search_content, in cursor.fetchall():
+        documents.append(search_content)
 
-stoplist = {}
-with open("stop_words.txt", "r") as fh:
-    for line in fh:
-        stoplist[line.strip()] = True
-        
-print "split"
-texts = []
-word_counter = {}
-for document in documents:
-    words = []
-    for word in document.lower().split():
-        if word not in stoplist and word[0] not in symbols \
-            and numeric.match(word) is None and numeric2.search(word) is None:
-            if word[-1] in ",.!?":
-                word = word[:-1]
-            result = double_quote.match(word)
-            if result is not None:
-                word = result.group(1)
-            result = kakko_quote.match(word)
-            if result is not None:
-                word = result.group(1)
+    symbols = "<>^[]!-/:@-`{-~,;*+|=.#$%&?\\"
+    numeric = re.compile("^[0-9\.%:,]+$")
+    numeric2 = re.compile("[0-9]")
+    double_quote = re.compile("\"([A-Za-z0-9]+)\"")
+    single_quote = re.compile("'([A-Za-z0-9]+)'")
+    kakko_quote = re.compile("\(([A-Za-z0-9]+)\)")
+
+    stoplist = {}
+    with open("stop_words.txt", "r") as fh:
+        for line in fh:
+            stoplist[line.strip()] = True
             
-            if word[0] in "\"\'(" or word[-1] in "\"\'(":
-                continue
-            if word[:2] == "0x":
-                continue
+    print "split"
+    texts = []
+    word_counter = {}
+    for document in documents:
+        words = []
+        for word in document.lower().split():
+            if word not in stoplist and word[0] not in symbols \
+                and numeric.match(word) is None and numeric2.search(word) is None:
+                if word[-1] in ",.!?":
+                    word = word[:-1]
+                result = double_quote.match(word)
+                if result is not None:
+                    word = result.group(1)
+                result = kakko_quote.match(word)
+                if result is not None:
+                    word = result.group(1)
                 
-            words.append(word)
-            if word not in word_counter:
-                word_counter[word] = 0
-            word_counter[word] += 1
-    texts.append(words)
+                if word[0] in "\"\'(" or word[-1] in "\"\'(":
+                    continue
+                if word[:2] == "0x":
+                    continue
+                    
+                words.append(word)
+                if word not in word_counter:
+                    word_counter[word] = 0
+                word_counter[word] += 1
+        texts.append(words)
 
-new_texts = []
-for text in texts:
-    row = []
-    for word, tag in nltk.pos_tag(text):
-        if tag in ['NN', 'NNS', 'NP', 'NPS']:
-            row.append(word)
-    new_texts.append(row)
-texts = new_texts
+    new_texts = []
+    for text in texts:
+        row = []
+        for word, tag in nltk.pos_tag(text):
+            if tag in ['NN', 'NNS', 'NP', 'NPS']:
+                row.append(word)
+        new_texts.append(row)
+    texts = new_texts
+    with open("texts.p", "w") as fh:
+        pickle.dump((texts, word_counter), fh)
+else:
+    with open("texts.p", "r") as fh:
+        texts, word_counter = pickle.load(fh)
 
 print "word"
 texts = [[word for word in text if word_counter[word] > 10]
@@ -80,9 +90,11 @@ else:
     
 print "bow"
 corpus = [dictionary.doc2bow(text) for text in texts]
-n_topics = 10
-lda = LDA(n_topics, 100)
+n_topics = 20
+lda = LDA(n_topics, 100, alpha=0.1, beta=0.1)
+t1 = time.time()
 phi, theta = lda.fit_transform(corpus)
+print("train time %.1fsec"%(time.time() - t1))
 for k in range(n_topics):
     print("topic:", k)
     indexes = np.argsort(phi[k])
